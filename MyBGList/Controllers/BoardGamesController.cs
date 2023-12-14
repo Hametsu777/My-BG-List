2f3d2021a25af066dbe39b200d4e0e77e901a701
Justin Moore <hametsu777@gmail.com>
2023-12-13 22:00:40 -0500

    Add project files.

---
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBGList.Models;
+
+namespace MyBGList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardGamesController : ControllerBase
+    {
+        private static List<BoardGame> boardGames = new List<BoardGame>
+        {
+            new BoardGame()
+            {
+                Id = 1,
+                Name = "Axis & Aliies",
+                Year = 1981,
+                MinPlayers = 2,
+                MaxPlayers = 5,
+            },
+            new BoardGame()
+            {
+                Id = 2,
+                Name = "Citadels",
+                Year = 2000,
+                MinPlayers = 2,
+                MaxPlayers = 8,
+            },
+            new BoardGame()
+            {
+                Id = 3,
+                Name = "Terraforming Mars",
+                Year = 2016,
+                MinPlayers = 1,
+                MaxPlayers = 5,
+            }
+    };
+        private readonly ILogger<BoardGamesController> _logger;
+
+        public BoardGamesController(ILogger<BoardGamesController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Name property is used for Url Generation (based on a specific route) and is not the same as routing.
+        // [HttpGet("/someName")] is not the same as [HttpGet( Name = "someName")].
+        [HttpGet("/GetBoardGames")]
+        public IEnumerable<BoardGame> GetBoardGames()
+        {
+            return boardGames;
+        }
+    }
+}
---
+++ MyBGList/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBGList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet("/error")]
+        public IActionResult Error()
+        {
+            return Problem();
+        }
+
+        [HttpGet("/error/test")]
+        public IActionResult Test()
+        {
+            throw new Exception("test");
+        }
+    }
+}
